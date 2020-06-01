88730125d05fa00e7fb3f52a7541da3ada3c004f
juannahuelhidalgo <juannahuelhidalgo@gmail.com>
2020-06-01 07:11:56 -0300

    ScriptsV1.0

---
+++ Collateral/Assets/Codigos/menu/menuPrinc.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//comentario 
+public class menuPrinc : MonoBehaviour
+{
+    public Texture2D[] frames0;
+    public double fps;
+    bool reproduciendo = false;
+
+    void Update()
+    {
+        StartCoroutine(menuReprod());
+    }
+
+    void Start()
+    {
+        menuReprod();
+    }
+
+    IEnumerator menuReprod()
+    {
+        int indice = (int)(Time.time * fps % frames0.Length);
+        GetComponent<RawImage>().texture = frames0[indice];
+        yield return new WaitForSeconds(1);
+        reproducido();
+    }
+
+    public void reproducido()
+    {
+        reproduciendo = true;
+    }
+}
