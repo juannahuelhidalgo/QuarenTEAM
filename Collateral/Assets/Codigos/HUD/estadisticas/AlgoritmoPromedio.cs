da2d0d70b06d9bca82ac7826a7cf9b0718859b19
EnzoDon <62855979+EnzoDon@users.noreply.github.com>
2020-06-07 10:32:57 -0300

    Update AlgoritmoPromedio.cs

--- Collateral/Assets/Codigos/HUD/estadisticas/AlgoritmoPromedio.cs
+++ Collateral/Assets/Codigos/HUD/estadisticas/AlgoritmoPromedio.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AlgoritmoPromedio : Algoritmo
 {
     public float Calcular(int[] respTomadas, int lugarArray, int correctas)
     {
-        return (float)(correctas/(lugarArray+1));
+        float correctastotales = (float)correctas;
+
+        float numero = correctastotales / (lugarArray + 1);
+
+        return (float) System.Math.Round(numero,2);
+        
     }
 }
