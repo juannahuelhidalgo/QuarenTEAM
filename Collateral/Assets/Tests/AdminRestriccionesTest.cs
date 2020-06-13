15f2b5198d77d431b9ea7ab3f63bd16b7e7c3b3d
Frank21111 <62523004+Frank21111@users.noreply.github.com>
2020-06-13 17:50:49 -0300

    implementacion preliminar de graficos

---
+++ Collateral/Assets/Tests/AdminRestriccionesTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class AdminRestriccionesTest
+    {
+        GameObject juego;
+        AdminRestricciones admin;
+
+
+        //Aqui se pondra lo que se inicia/instancia con el comienzo de cada test
+        [SetUp]
+        public void Setup()
+        {
+
+            juego = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Escenas/Juego"));
+
+            admin = GameObject.Find("CanvasRestricciones").GetComponent<AdminRestricciones>();
+        }
+
+        [UnityTest]
+        public IEnumerator setRestriccionesTest()
+        {
+            bool ResultadoTest = true;
+            int i = GameObject.Find("adminJuegos").GetComponent<adminJuego>().getSemanaActual();
+            string semanaActual = "Semana" + i;
+            // UnityEngine.Debug.Log(nombre);
+        
+            //GameObject.Find("CanvasRestricciones").GetComponent<Canvas>().enabled = true;
+            yield return new WaitForSeconds(3f);
+            
+            admin.setRestricciones();
+            /*for(int j=1; j<5; j++)
+            {
+                string semana = "Semana" + j;
+                if (j != i)
+                    if (GameObject.Find(semana).active)
+                        ResultadoTest = false;
+            }
+            if (!GameObject.Find(semanaActual).active)
+                ResultadoTest = false;
+
+            Assert.IsTrue(ResultadoTest);*/
+            yield return null;
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            //Se destruyen los objetos 
+            GameObject.Destroy(juego);
+
+        }
+    }
+
+}
---
+++ Collateral/Assets/Tests/datosPacientesTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class datosPacientesTest
+    {
+        private GameObject juego;
+        private datosPacientes datos;
+        // A Test behaves as an ordinary method
+        [SetUp]
+        public void Setup()
+        {
+            // Use the Assert class to test conditions
+            juego = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Escenas/Juego"));
+            datos = GameObject.Find("Documento").GetComponent<datosPacientes>();
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator generatePatientTest()
+        {
+            string[] inforPacienteAnterior = new string[6];
+            string[] inforPacientesActual = new string[6];
+
+            inforPacienteAnterior = datos.generatePatient();
+            inforPacientesActual = datos.generatePatient();
+
+            Assert.IsFalse(inforPacienteAnterior.Equals(inforPacientesActual));
+
+            yield return null;
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            GameObject.Destroy(juego);
+        }
+    }
+}
